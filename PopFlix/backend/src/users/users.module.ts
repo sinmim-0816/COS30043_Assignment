099f7751f46bada113352d6d37d6221b5b8c4796
sinmim-0816 <janefam88@gmail.com>
2026-05-31 23:18:18 +0800

    fix email

--- PopFlix/backend/src/users/users.module.ts
+++ PopFlix/backend/src/users/users.module.ts
@@ -5,9 +5,10 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 import { NotificationModule } from 'src/notification/notification.module';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), MailerModule, NotificationModule],
+  imports: [TypeOrmModule.forFeature([User]), MailerModule, NotificationModule, EmailModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
