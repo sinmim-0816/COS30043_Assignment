19612282f811da7afc8cddf5201a77adbcdd0bac
sinmim-0816 <janefam88@gmail.com>
2026-06-06 15:03:51 +0800

    yout

--- PopFlix/frontend/src/api/client.js
+++ PopFlix/frontend/src/api/client.js
@@ -1,7 +1,6 @@
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = "c9858d1981c48307a734e8bb777a1a5c";
-// const YOUTUBE_API_KEY = "AIzaSyBo2ivSxIJmHJRd4HmnmqkrrdX9tqNySe8";
-const YOUTUBE_API_KEY = "AI";
+const YOUTUBE_API_KEY = "AIzaSyBo2ivSxIJmHJRd4HmnmqkrrdX9tqNySe8";
 
 // ============================
 // TMDB API CLIENT
